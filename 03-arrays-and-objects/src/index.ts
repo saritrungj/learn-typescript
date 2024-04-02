504d17ad32012f9f55c275422f93eae3e3a227c2
Sarit 'TAWAN' Rungjapoh <107798657+saritrungj@users.noreply.github.com>
2024-04-02 15:03:16 +0700

    Learn basic and concept typescript

---
+++ 03-arrays-and-objects/src/index.ts
@@ -0,0 +1,67 @@
+let stringArr = ['one', 'hey', 'Sarit']
+
+let guitars = ['Start', 'Les Paul', 5150]
+
+let mixedData = ['EVH', 1984, true]
+
+stringArr[0] = 'John'
+stringArr.push('hey')
+
+guitars[0] = 1984
+guitars.unshift('Jim')
+
+guitars = stringArr
+mixedData = guitars
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+// Tuple
+let myTuple: [string, number, boolean] = ['Sarit', 42, true]
+
+let mixed = ['John', 1, false]
+
+myTuple[1] = 42
+
+// Objects
+let myObj: object
+myObj = []
+console.log(typeof myObj);
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+  prop1: 'Sarit',
+  prop2: true,
+}
+
+
+exampleObj.prop1 = 'Sarit'
+exampleObj.prop2 = false
+
+type Guitarist = {
+  name: string,
+  active: boolean,
+  albums: (string | number)[],
+}
+
+let evh: Guitarist = {
+  name: 'Eddie',
+  active: false,
+  albums: [1984, 5150, 'OU812']
+}
+
+let jp: Guitarist = {
+  name: 'Jimmy',
+  active: true,
+  albums: ['I', 'II', 'IV']
+}
+
+evh = jp
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  return `Hello ${guitarist.name}!`
+}
+
+console.log(greetGuitarist(evh));
